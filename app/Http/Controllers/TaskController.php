e94a668bcfbbe6a4864b74ad82e7bc30cd7ed4ee
sohelsamy1git remote add origin https://github.com/sohelsamy1/portfo.gitgit remote add origin https://github.com/sohelsamy1/portfo.git <sumon33021@gmail.com>
2026-03-04 12:58:21 +0600

    feat: connect create form to store task in database

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -3,22 +3,50 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TaskController extends Controller
 {
-    public function index()
+
+   public function index()
     {
         return view('tasks.index');
     }
+
 
     public function create()
     {
         return view('tasks.create');
     }
 
-     public function edit()
+
+    public function store(Request $request)
     {
-           return view('tasks.edit');
+    $fileParth = null;
+
+    if ($request->hasFile('image')) {
+
+        $file = $request->file('image');
+        $fileName = 'img' . time() . '_' . $file->getClientOriginalName();
+        $fileParth = $file->storeAs('newFolder', $fileName, 'public');
+    }
+
+    DB::table('tasks')->insert([
+        'title'       => $request->title,
+        'description' => $request->description,
+        'image'       => $fileParth,
+        'created_at'  => now(),
+        'updated_at'  => now(),
+    ]);
+
+    return redirect()->route('tasks.index')
+                     ->with('success', 'Task created successfully');
+    }
+
+
+    public function edit()
+    {
+        return view('tasks.edit');
     }
 
 }
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [TaskController::class, 'index'])->name('tasks.index');
 Route::get('/tasks/create', [TaskController::class, 'create'])->name('tasks.create');
+Route::post('/tasks', [TaskController::class, 'store'])->name('tasks.store');
 
 Route::get('/tasks/{id}/edit', [TaskController::class, 'edit'])->name('tasks.edit');
